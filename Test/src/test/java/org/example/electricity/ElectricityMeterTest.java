cd7b1e350a0196afebead780dc24c7b482609cba
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-14 19:48:24 +0100

    Severa test

--- Test/src/test/java/org/example/electricity/ElectricityMeterTest.java
+++ Test/src/test/java/org/example/electricity/ElectricityMeterTest.java
@@ -1,6 +1,8 @@
 package org.example.electricity;
 
 import org.junit.Assert;
+import org.junit.Test;
+
 
 
 public class ElectricityMeterTest {
@@ -11,6 +13,30 @@ public class ElectricityMeterTest {
         ElectricityMeter electricityMeter = new ElectricityMeter();
         electricityMeter.addKwh(1);
         Assert.assertTrue(electricityMeter.getKwh() == 1);
+
+    }
+
+    //Method, context, behavior
+    @Test
+    public void addKwh_newMeter7_properAddition() {
+
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(2);
+        electricityMeter.addKwh(5);
+        Assert.assertTrue(electricityMeter.getKwh() == 7);
+
+    }
+
+    @Test
+    public void addKwh_newMeter11_properAddition() {
+
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(2);
+        electricityMeter.addKwh(3);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 11);
 
     }
 }
